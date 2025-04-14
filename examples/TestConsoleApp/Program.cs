822eec775a4c1d1b91c524dcc52490ec6efef0f6
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-04-14 16:50:19 +0200

    .

--- examples/TestConsoleApp/Program.cs
+++ examples/TestConsoleApp/Program.cs
@@ -15,9 +15,9 @@ class Program
         //new RamlConverter().ConvertToFile("Examples\\MuleSoft\\test.raml", Path.Combine(DestFolder, "MuleSoft.converted.json"));
         // new RamlConverter().ConvertToFile("Examples\\InheritedDatatype\\simpleinherited.raml", Path.Combine(DestFolder, "simpleinherited.json"));
 
-        new RamlConverter().ConvertToFile("Examples\\HelloWorld.raml", Path.Combine(DestFolder, "HelloWorld.converted.json"));
+        //new RamlConverter().ConvertToFile("Examples\\HelloWorld.raml", Path.Combine(DestFolder, "HelloWorld.converted.json"));
         //new RamlConverter().ConvertToFile("Examples\\IncludePerson\\api.raml", Path.Combine(DestFolder, "IncludePerson\\api.converted.json"));
-        //new RamlConverter().ConvertToFile("Examples\\MediaWiki.raml", Path.Combine(DestFolder, "MediaWiki.converted.json"));
+        new RamlConverter().ConvertToFile("Examples\\MediaWiki.raml", Path.Combine(DestFolder, "MediaWiki.converted.json"));
         Console.WriteLine("DONE");
     }
 }
