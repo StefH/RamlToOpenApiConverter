826253c4aea7f2d729c6cd82b271dd6ddae75def
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-05-03 10:06:49 +0200

    ...

---
+++ src/RamlToOpenApiConverter.SourceOnly/Constants.cs
@@ -0,0 +1,8 @@
+namespace RamlToOpenApiConverter;
+
+internal static class Constants
+{
+    public const string IncludeTag = "!include";
+    public const string IsTag = "is";
+    public const string Traits = "traits";
+}
