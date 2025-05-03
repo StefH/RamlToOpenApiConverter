82f796541fcdd52f017129462b72f121b1f64493
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-05-03 10:13:25 +0200

    no ilpack

--- src/RamlToOpenApiConverter.SourceOnly/Constants.cs
+++
@@ -1,8 +0,0 @@
-namespace RamlToOpenApiConverter;
-
-internal static class Constants
-{
-    public const string IncludeTag = "!include";
-    public const string IsTag = "is";
-    public const string Traits = "traits";
-}
