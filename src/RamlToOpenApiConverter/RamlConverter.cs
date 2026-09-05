ba39a1accd69e0a4b62d4615b804ad0d1bbc9b58
Stef Heyenrath <stef.heyenrath@gmail.com>
2026-09-05 17:36:03 +0200

    .

--- src/RamlToOpenApiConverter/RamlConverter.cs
+++ src/RamlToOpenApiConverter/RamlConverter.cs
@@ -10,9 +10,9 @@ namespace RamlToOpenApiConverter;
 
 public partial class RamlConverter
 {
-    private readonly Dictionary<object, object> _typesAsRef = new Dictionary<object, object>();
-    private readonly Dictionary<object, object> _types = new Dictionary<object, object>();
-    private readonly Dictionary<object, object> _uses = new Dictionary<object, object>();
+    private readonly Dictionary<object, object> _typesAsRef = new();
+    private readonly Dictionary<object, object> _types = new();
+    private readonly Dictionary<object, object> _uses = new();
 
     private IDeserializer _deserializer = null!;
     private OpenApiDocument _doc = null!;
